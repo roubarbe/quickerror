6b4af4c41a4d6a59375f236c760078eb6a5820e4
Maya Provost <propasc@gmail.com>
2026-07-17 00:36:12 -0400

    First commit

---
+++ msgErreur.js
@@ -0,0 +1,170 @@
+class ErrorMessage extends HTMLElement {
+	static observedAttributes = ["errtitle", "errmessage"];
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.innerHTML = `
+			<style>
+				:host {
+				  display: none;
+				}
+				:host([onscreen]) {
+				  display: flex;
+				}
+				#error{
+					display: flex;
+					flex-direction: row;
+					position: absolute;
+					align-items:stretch;
+					background-color: #b8324a;
+					border-radius: 0.33rem;
+					z-index: 1000;
+					box-shadow: 0 0 30px -10px #000;
+					left: 50%;
+				    top: 33%;
+				    transform: translateX(-50%);
+				    width: min(500px, 90vw);
+				    animation: fadeInUp 0.2s ease-in-out;
+				}
+				#error.closing {
+				   animation: fadeOutUp 0.2s ease-in-out forwards;
+				}
+				@keyframes fadeOutUp {
+				   from { opacity: 1; transform: translateX(-50%) translateY(0); }
+				   to { opacity: 0; transform: translateX(-50%) translateY(-30px); }
+				}
+				@keyframes fadeInUp {
+				   from { opacity: 0; transform: translateX(-50%) translateY(30px); }
+				   to { opacity: 1; transform: translateX(-50%) translateY(0); }
+				}
+				#message{
+					flex-grow: 1;
+					font-size: 0.8rem;
+					color: #ffdde0;
+				}
+				#main{
+					display: flex;
+					padding: 1rem;
+					flex-direction: column;
+					align-items: center;
+					width: 66%;
+					border-left: 1px solid #da5362;
+					text-align: center;
+				}
+				button {
+				  padding: 0.66rem 1.66rem;
+				  border-radius: 0.33rem;
+				  font-size: 0.75rem;
+				  font-weight: bold;
+				  text-transform: uppercase;
+				  width: 66%;
+				  margin-top: 2rem;
+				  background-color: transparent;
+				  border: 1pt solid #ffb3bb;
+				  color: #ffb3bb;
+				  transition: background-color 0.1s ease-in-out, color 0.1s ease-in-out;
+				}
+				button:hover{
+					background-color: #ffb3bb;
+				    color: #431a1e;
+					cursor: pointer;
+				}
+				svg{
+					width: 60%;
+				}
+				#gfx{
+					width: 33%;
+					border-radius: 0.33rem 0 0 0.33rem;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					justify-content: center;
+					background-image: linear-gradient(#c94f66, #b8324a);
+				}
+				#title{
+					font-size: 1.1rem;
+					color: #FFF;
+					padding-top: 0.33rem;
+					padding-bottom: 0.33rem;
+					font-weight: 700;
+				}
+			</style>
+			<div id="error">
+				<div id="gfx">
+					<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+						<title/>
+						<g id="Complete">
+							<g id="x-circle">
+								<g>
+									<circle 
+										cx="12" cy="12" data-name="--Circle" fill="none" id="_--Circle" r="10" 
+										stroke="#431a1e" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+									/>
+									<line 
+										fill="none" stroke="#431a1e" stroke-linecap="round" stroke-linejoin="round" 
+										stroke-width="2" x1="14.5" x2="9.5" y1="9.5" y2="14.5"
+									/>
+									<line 
+										fill="none" stroke="#431a1e" stroke-linecap="round" stroke-linejoin="round" 
+										stroke-width="2" x1="14.5" x2="9.5" y1="14.5" y2="9.5"
+									/>
+								</g>
+							</g>
+						</g>
+					</svg>
+				</div>
+				<div id="main">
+					<div id="title"></div>
+					<div id="message"></div>
+					<button>Close</button>
+				</div>
+			</div>
+		`;
+
+		this.shadowRoot.querySelector("#error button").addEventListener('click', e => {
+			const error = this.shadowRoot.querySelector("#error");
+			error.classList.add("closing");
+			error.addEventListener("animationend", () => {
+				this.onscreen = false;
+				error.classList.remove("closing");
+			}, { once: true });
+		});
+	}
+
+	attributeChangedCallback(nom, avant, apres) {
+		if (nom === "errmessage") {
+			this.shadowRoot.querySelector("#message").textContent = apres ?? "Please define an error message";
+		}
+
+		if(nom === "errtitle"){
+			this.shadowRoot.querySelector("#title").textContent = apres ?? "Please define a title";
+		}
+	}
+
+	get errmessage() {
+		return this.getAttribute("errmessage");
+	}
+
+	set errmessage(val) {
+		this.setAttribute("errmessage", val);
+	}
+
+	get errtitle(){
+		return this.getAttribute("errtitle");
+	}
+
+	set errtitle(val) {
+		this.setAttribute("errtitle", val);
+	}
+
+	get onscreen() {
+		return this.hasAttribute("onscreen");
+	}
+	set onscreen(val) {
+		if (val) this.setAttribute("onscreen", "");
+		else this.removeAttribute("onscreen");
+	}
+}
+
+customElements.define("error-msg", ErrorMessage);
